10aba9f7348ba86ca157bb0e45b31fba0aa5e94f
bangranggas <bangranggas20@gmail.com>
2025-03-30 03:46:23 +0700

    some fixing

--- scraping/urls.py
+++ scraping/urls.py
@@ -4,7 +4,7 @@ from scraping.views import show_webdrive, show_webof, scrape_drive_data_json, sc
 app_name = 'scraping'
 
 urlpatterns = [
-    path('drive/', show_webdrive, name='show_webdrive'),
+    path('', show_webdrive, name='show_webdrive'),
     path('of/', show_webof, name='show_webof'),
     path('scrape-data-drive/', scrape_drive_data_json, name='scrape_data_drive'),
     path('scrape-data-of/', scrape_of_data_json, name='scrape_data_of'),
