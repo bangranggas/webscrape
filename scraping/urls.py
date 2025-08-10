2a2909039c06490f8bd03ca8dce2061350375884
jeremiaranggasetyawan <jeremiaranggas@gmail.com>
2025-08-10 22:38:02 +0700

    redesign

--- scraping/urls.py
+++ scraping/urls.py
@@ -4,10 +4,10 @@ from scraping.views import show_webdrive, show_webof,show_webofsearch, scrape_dr
 app_name = 'scraping'
 
 urlpatterns = [
-    path('', show_webdrive, name='show_webdrive'),
-    path('of/', show_webof, name='show_webof'),
-    path('ofsearch/', show_webofsearch, name='show_webofsearch'),
+    path('gdrive/', show_webdrive, name='show_webdrive'),
+    path('', show_webof, name='show_webof'),
+    path('custom', show_webofsearch, name='show_webofsearch'),
     path('scrape-data-drive/', scrape_drive_data_json, name='scrape_data_drive'),
     path('scrape-data-of/', scrape_of_data_json, name='scrape_data_of'),
-    path('scrape-search-data/', scrape_of_search_data_json, name='scrape_search_data'),
+    path('scrape-data-custom/', scrape_of_search_data_json, name='scrape_search_data'),
 ]
